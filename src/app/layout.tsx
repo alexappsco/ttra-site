823f36659481592519fe6df9e77ef8087fd490aa
haassanbash <hassanbasheer135@gmail.com>
2025-11-25 08:33:57 +0200

    fixed speed in mobile tt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
 
+
+
 import React from 'react';
+import 'simplebar-react/dist/simplebar.min.css'; // تحميل بدون render-blocking
+
+import '../global.css';
 
 export const viewport = {
   themeColor: '#000000',
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 5, // اختياري — أو احذفها لتحسين أكثر
 };
-
 
 export const metadata = {
   title: 'Isthwath | منصة استحواذ',
   description: `استحواذ هي منصة سعودية رائدة تمكّنك من بيع وشراء الأنشطة التجارية والمشاريع الصغيرة بكل سهولة وشفافية.
-تم تصميم المنصة لتكون نقطة التقاء بين رواد الأعمال، المستثمرين، وأصحاب المشاريع الباحثين عن فرص جاهزة للانطلاق أو التوسع داخل السوق السعودي.
-
-من خلال تطبيق استحواذ - Isthwath، يمكنك إدارة عملية البيع أو الشراء بشكل متكامل داخل بيئة رقمية مرنة وآمنة.`,
+تم تصميم المنصة لتكون نقطة التقاء بين رواد الأعمال، المستثمرين، وأصحاب المشاريع الباحثين عن فرص جاهزة للانطلاق أو التوسع داخل السوق السعودي.`,
   openGraph: {
     url: 'https://isthwath.com/ar/',
     title: 'Isthwath | منصة استحواذ',
@@ -32,27 +33,15 @@ export const metadata = {
   },
 };
 
-
-export default function Layout({ children, auth }: { children: React.ReactNode; auth: React.ReactNode }) {
+export default function Layout({
+  children,
+  auth,
+}: {
+  children: React.ReactNode;
+  auth: React.ReactNode;
+}) {
   return (
-    <html>
-      <head>
-        {/* تحسين LCP عبر تحميل الخطوط مسبقًا */}
-        <link
-          rel="preload"
-          href="/fonts/DINNextLTArabic-Regular.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/DINNextLTArabic-Bold.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
-      </head>
+    <html lang="ar" dir="rtl">
       <body>
         {children}
         {auth}
@@ -60,6 +49,69 @@ export default function Layout({ children, auth }: { children: React.ReactNode; 
     </html>
   );
 }
+
+///
+// import React from 'react';
+
+// export const viewport = {
+//   themeColor: '#000000',
+//   width: 'device-width',
+//   initialScale: 1,
+//   maximumScale: 5, // اختياري — أو احذفها لتحسين أكثر
+// };
+
+
+// export const metadata = {
+//   title: 'Isthwath | منصة استحواذ',
+//   description: `استحواذ هي منصة سعودية رائدة تمكّنك من بيع وشراء الأنشطة التجارية والمشاريع الصغيرة بكل سهولة وشفافية.
+// تم تصميم المنصة لتكون نقطة التقاء بين رواد الأعمال، المستثمرين، وأصحاب المشاريع الباحثين عن فرص جاهزة للانطلاق أو التوسع داخل السوق السعودي.
+
+// من خلال تطبيق استحواذ - Isthwath، يمكنك إدارة عملية البيع أو الشراء بشكل متكامل داخل بيئة رقمية مرنة وآمنة.`,
+//   openGraph: {
+//     url: 'https://isthwath.com/ar/',
+//     title: 'Isthwath | منصة استحواذ',
+//     description: `استحواذ هي منصة سعودية رائدة تمكّنك من بيع وشراء الأنشطة التجارية والمشاريع الصغيرة بكل سهولة وشفافية.`,
+//   },
+//   manifest: '/manifest.json',
+//   icons: {
+//     icon: [
+//       { rel: 'icon', type: 'image/svg+xml', url: '/logo/logo_blue.svg' },
+//       { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon/favicon-96x96.png' },
+//       { rel: 'apple-touch-icon', sizes: '32x12', url: '/favicon/apple-touch-icon.png' },
+//       { rel: 'apple-touch-icon', sizes: '180x180', url: '/logo/logo_blue.svg' },
+//     ],
+//     shortcut: { url: '/logo/logo_blue.svg' },
+//   },
+// };
+
+
+// export default function Layout({ children, auth }: { children: React.ReactNode; auth: React.ReactNode }) {
+//   return (
+//     <html>
+//       <head>
+//         {/* تحسين LCP عبر تحميل الخطوط مسبقًا */}
+//         <link
+//           rel="preload"
+//           href="/fonts/DINNextLTArabic-Regular.ttf"
+//           as="font"
+//           type="font/ttf"
+//           crossOrigin="anonymous"
+//         />
+//         <link
+//           rel="preload"
+//           href="/fonts/DINNextLTArabic-Bold.ttf"
+//           as="font"
+//           type="font/ttf"
+//           crossOrigin="anonymous"
+//         />
+//       </head>
+//       <body>
+//         {children}
+//         {auth}
+//       </body>
+//     </html>
+//   );
+// }
 // export const metadata = {
 //   title: 'Isthwath | منصة استحواذ',
 //  description: `استحواذ هي منصة سعودية رائدة تمكّنك من بيع وشراء الأنشطة التجارية والمشاريع الصغيرة بكل سهولة وشفافية.
